4cc4cb85932e45fac1c4b82bffeab767f782fc9f
Will Heath <76233664+wtheathOTG@users.noreply.github.com>
2024-11-08 11:34:18 -0800

    Fixed Pose2D for GoBildaPinpointDriver.java by updating versions in gradle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/GoBildaPinpointDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/GoBildaPinpointDriver.java
@@ -20,7 +20,7 @@
  *   SOFTWARE.
  */
 
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.bot;
 
 import static com.qualcomm.robotcore.util.TypeConversion.byteArrayToInt;
 
