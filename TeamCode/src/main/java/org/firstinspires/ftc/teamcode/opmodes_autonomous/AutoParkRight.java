0d23fc3c0d703165027ceecd9e8daf0225d8fbed
Will Heath <76233664+wtheathOTG@users.noreply.github.com>
2024-11-21 16:53:35 -0800

    Add AutoParkRight

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes_autonomous/AutoParkRight.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.opmodes_autonomous;
+
+import static org.firstinspires.ftc.teamcode.auto_execution.AutoActions.MOVE;
+import static org.firstinspires.ftc.teamcode.auto_execution.AutoActions.WAIT;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.auto_execution.AutoActionHandler;
+import org.firstinspires.ftc.teamcode.bot.Robot;
+import org.firstinspires.ftc.teamcode.util.MathHelper;
+import org.firstinspires.ftc.teamcode.util.TeleSingle;
+
+//import org.firstinspires.ftc.teamcode.bot.Robot;
+
+@Autonomous
+public class AutoParkRight extends LinearOpMode {
+    Robot robot;
+    AutoActionHandler actionHandler;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot = new Robot(hardwareMap, telemetry);
+
+        TeleSingle.init(telemetry);
+
+        actionHandler = new AutoActionHandler(robot, telemetry);
+
+        actionHandler.add(WAIT,false,0.25);
+        actionHandler.add(MOVE,false, MathHelper.tilesToMM(1.4), 20, 0);
+
+        actionHandler.init();
+
+        waitForStart();
+
+        while (opModeIsActive()){
+            actionHandler.run();
+
+            //robot.positionTelemetry();
+
+            telemetry.update();
+        }
+    }
+}
